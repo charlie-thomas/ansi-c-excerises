011942dc6971dd988a2eb81356c2bc7313ef4feb
Charlie Thomas <charlie.thomas321@gmail.com>
2017-09-16 14:21:20 +0100

    [1.13] Added program to print a histogram of the lengths of words in its input

---
+++ Chapter-1/1.13-CountWordLengths.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define NO_WORDS 10 /* Number of words */
+
+int main() {
+    int c, count;
+    int lengths[NO_WORDS];
+
+    /* Initialise array */
+    for (int i = 0; i < NO_WORDS; i++)
+        lengths[i] = 0;
+
+    /* Count lengths of each word */
+    count = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\t' || c == '\n')
+            count++;
+        else
+            lengths[count]++;
+    }
+
+    /* Print out histogram of word lengths */
+    for (int i = 0; i < NO_WORDS; i++) {
+        for (int j = 0; j < lengths[i]; j++)
+            putchar('*');
+        putchar('\n');
+    }
+
+}
