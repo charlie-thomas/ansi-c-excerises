bd722ad5c781e55b6a4122b6f4d17535f95d3d24
Charlie Thomas <charlie.thomas321@gmail.com>
2017-09-19 15:39:51 +0100

    [2.4] Added an alternative version of squeeze(s1, s2) that deletes each character in s1 that matches anycharacter in s2

---
+++ Chapter-2/2.4-SqueezeStrings.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void squeeze(char s1[], char s2[]);
+void squeezeChar(char s[], int c);
+    
+int main() {
+    char s1[] = "Hello";
+    char s2[] = "Charlie";
+
+    printf("String 1: %s\nString 2: %s\n", s1, s2);
+    squeeze(s1, s2);
+    printf("Squeezed: %s\n", s1);
+}
+
+/* squeeze: deleted each character in s1 that matches any character in s2 */
+void squeeze(char s1[], char s2[]) {
+    for (int i = 0; s2[i] != '\0'; i++)
+        squeezeChar(s1, s2[i]);
+}
+
+/* squeezeChar: delete all c from s (from book) */
+void squeezeChar(char s[], int c) {
+    int i, j;
+    for (i = j = 0; s[i] != '\0'; i++)
+        if (s[i] != c)
+        s[j++] = s[i];
+    s[j] = '\0';
+}
