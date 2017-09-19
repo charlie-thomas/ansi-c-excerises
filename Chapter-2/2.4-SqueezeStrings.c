5fd825d713a62749f6343290c8bf7f0c1ca9ce19
Charlie Thomas <charlie.thomas321@gmail.com>
2017-09-19 15:47:27 +0100

    [2.4] Fixed indentation

--- Chapter-2/2.4-SqueezeStrings.c
+++ Chapter-2/2.4-SqueezeStrings.c
@@ -23,6 +23,6 @@ void squeezeChar(char s[], int c) {
     int i, j;
     for (i = j = 0; s[i] != '\0'; i++)
         if (s[i] != c)
-        s[j++] = s[i];
+            s[j++] = s[i];
     s[j] = '\0';
 }
